d297a0be0ed09404d3581e3d5b7e04f7fecbb300
Shu045 <shubhamkharde45@gmail.com>
2026-07-07 08:58:31 +0530

    feature:added install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+VERSION="v1.0.0"
+URL="https://github.com/<username>/<repo>/releases/download/${VERSION}/prmgr"
+
+INSTALL_DIR="$HOME/.prmgr/bin"
+
+mkdir -p "$INSTALL_DIR"
+
+echo "Downloading..."
+curl -L "$URL" -o "$INSTALL_DIR/prmgr"
+
+chmod +x "$INSTALL_DIR/prmgr"
+
+echo "Installed to $INSTALL_DIR"
+
+if [[ ":$PATH:" != *":$INSTALL_DIR:"* ]]; then
+    echo ""
+    echo 'export PATH="$HOME/.prmgr/bin:$PATH"' >> ~/.bashrc
+    echo "Added ~/.prmgr/bin to PATH"
+    echo "Restart your terminal or run:"
+    echo "source ~/.bashrc"
+fi
+
+echo "Done! Run:"
+echo "prmgr"
